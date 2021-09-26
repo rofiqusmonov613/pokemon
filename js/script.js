8a28ca93f6b5bd8af90eb1a7b776938c056c589c
rofiqusmonov613 <rofiqusmonov613@gmail.com>
2021-09-26 18:07:38 +0500

    pokemons

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 const elList = document.querySelector('.films__card-wrapper');
+const elCheck = document.querySelector('.button');
 const elForm = document.querySelector('.form');
 const elInputSearch = selectElem('.films__input-serach', elForm);
 const elSelect = selectElem('.films__select', elForm);
+const elDiv = document.querySelector('.modal')
+const elBtn = document.querySelector('.btn')
 const elFilter = selectElem('.films__filter', elForm);
-const elTemplate = document.querySelector('#template').content
+const elTemplate = document.querySelector('#template').content;
+const elDivWrapper = document.querySelector('.modal-wrapper');
+const elIcon = document.querySelector('.btn-icon');
+const elNewLi = document.querySelector('.films__card');
+const elLi = document.querySelector('.pokemons__card');
+const elImg = document.querySelector('.pokemons__img');
+const elName = document.querySelector('.pokemons__card-title');
+const elType = document.querySelector('.pokemons__card-genre');
+const elKg = document.querySelector('.pokemons__kg');
+const elAge = document.querySelector('.pokemons__release-date');
+const elNewImg = document.querySelector('.films__img');
+const elNewTitle = document.querySelector('.films__card-title');
 
 function renderMovies(pokemonsArr, element){
     element.innerHTML = null;
@@ -14,7 +28,8 @@ function renderMovies(pokemonsArr, element){
         selectElem('.films__img', cloneTemplate).src = pokemon.img
         selectElem('.films__card-title', cloneTemplate).textContent = pokemon.name
         selectElem('.films__card-genre', cloneTemplate).textContent = pokemon.type
-        selectElem('.films__release-date', cloneTemplate).textContent = normalizeDate(pokemon.num)
+        selectElem('.films__kg', cloneTemplate).textContent = pokemon.egg;
+        selectElem('.films__release-date', cloneTemplate).textContent = pokemon.avg_spawns;
         
         element.appendChild(cloneTemplate);
     })
@@ -86,9 +101,9 @@ elForm.addEventListener('submit', (e) =>{
             })
     }else if(filterValue === 'old__new'){
         foundFilms.sort((a, b) =>{
-            if(a.num > b.num){
+            if(a.avg_spawns > b.avg_spawns){
                 return 1
-            }else if(a.num < b.num){
+            }else if(a.avg_spawns < b.avg_spawns){
                 return -1
             }else{
                 return 0
@@ -96,9 +111,9 @@ elForm.addEventListener('submit', (e) =>{
         })
 }else if(filterValue === 'new__old'){
     foundFilms.sort((b, a) =>{
-        if(a.num > b.num){
+        if(a.avg_spawns > b.avg_spawns){
             return 1
-        }else if(a.num < b.num){
+        }else if(a.avg_spawns < b.avg_spawns){
             return -1
         }else{
             return 0
@@ -110,3 +125,28 @@ elForm.addEventListener('submit', (e) =>{
 
     renderMovies(foundFilms, elList);
 });
+elCheck.addEventListener('click', function() {
+
+    if(elCheck) {
+        elDiv.style.display = 'flex';
+        elDivWrapper.style.display = 'block';
+        
+    }
+})
+elBtn.addEventListener('click', function() {
+
+    if(elBtn) {
+        elDiv.style.display = 'none';
+        elDivWrapper.style.display = 'none';
+    }
+})
+elDiv.addEventListener('click', function() {
+
+    if(elDiv) {
+        elDiv.style.display = 'none';
+        elDivWrapper.style.display = 'none';
+    }
+})
+elIcon.addEventListener('click',function() {
+    
+})
